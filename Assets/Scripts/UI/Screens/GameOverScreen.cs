aa0070009a0f99ad42a4fd34da34d84b680d6a29
Dimitry Ribeiro <dimitrylopes.jogos@gmail.com>
2025-12-21 02:07:17 -0300

    added game over screen

---
+++ Assets/Scripts/UI/Screens/GameOverScreen.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : UIScreen<GameOverScreenController>
+{
+    [SerializeField]
+    private TextMeshProUGUI finalScoreText;
+    [SerializeField]
+    private Button roomScreenButton;
+    [SerializeField]
+    private Button lobbyScreenButton;
+    [SerializeField]
+    private Button mainMenuButton;
+
+    private void OnRoomScreenButtonClick()
+    {
+
+    }
+
+    private void OnLobbyScreenButtonClick()
+    {
+
+    }
+
+    private void OnMainMenuGameButtonClick()
+    {
+
+    }
+}
